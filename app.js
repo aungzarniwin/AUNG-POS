aef39c760edb4cf950bf9a504820a4db6685a38b
aungzarniwin <42805519+aungzarniwin@users.noreply.github.com>
2026-09-10 11:39:47 +0630

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+function showMessage(page) {
+  alert(page + " module is coming soon!");
+}
+
+function upgradePro() {
+  alert(
+    "Aung POS PRO\n\n" +
+    "Unlock unlimited products, advanced reports, " +
+    "customer credit, staff accounts and more."
+  );
+}
